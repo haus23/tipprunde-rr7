80c16973fa68abdb412a19c24daf67067096fb92
Micha <micha@haus23.net>
2024-09-29 13:34:31 +0200

    feat: Move home route.

---
+++ app/routes/foh/tables/_route.tsx
@@ -0,0 +1,12 @@
+export const meta = [
+  { title: 'Tabelle - runde.tips' },
+  { name: 'description', value: 'Tabelle der Haus23 Tipprunde' },
+];
+
+export default function TablesRoute() {
+  return (
+    <div>
+      <h1 className="text-2xl">Tabelle</h1>
+    </div>
+  );
+}
