1fe85e427a28253354b61dbfb7fd7c277261d7b7
Micha <micha@haus23.net>
2024-10-02 11:32:23 +0200

    feat: Generate code and send it to user.

--- app/utils/.server/auth.ts
+++ app/utils/.server/auth.ts
@@ -1,17 +1,17 @@
+import { generateTOTP } from '@epic-web/totp';
 import { redirect } from 'react-router';
 
 import { db } from './db';
 import { sendMail } from './email';
 
 /**
- * Validates email address against the user database
+ * Gets user by email address
  *
- * @param email Email to validate
- * @returns False for unknown email address - otherwise true
+ * @param email Known email address
+ * @returns Null if email address is unknown
  */
-async function isKnownEmail(email: string) {
-  const user = await db.user.findUnique({ where: { email } });
-  return user !== null;
+async function getUserByEmail(email: string) {
+  return await db.user.findUnique({ where: { email } });
 }
 
 /**
@@ -29,6 +29,51 @@ async function sendErrorMail(props: { subject: string; text: string }) {
 }
 
 /**
+ * Sends email with login code to user
+ *
+ * @param props Username, email-address and the login code
+ */
+async function sendCodeMail(props: {
+  userName: string;
+  code: string;
+  email: string;
+}) {
+  const { userName, code, email } = props;
+  await sendMail(
+    {
+      from: 'Tipprunde <hallo@runde.tips>',
+      to: `${userName} <${email}>`,
+      subject: 'Tipprunde Login Code',
+      category: 'totp',
+      text: `
+      Hallo ${userName}!
+      Dein Login-Code ist: ${code}`,
+    },
+    'Postmark',
+  );
+}
+
+/**
+ * Generates and stores TOTP login code.
+ *
+ * @param email User email the code will be associated with
+ * @returns Code
+ */
+async function createLoginCode(email: string) {
+  const { otp, secret, period, charSet, digits, algorithm } =
+    await generateTOTP({
+      period: 300,
+    });
+  const expiresAt = new Date(Date.now() + period * 1000);
+  await db.verification.upsert({
+    where: { email },
+    create: { email, secret, period, algorithm, digits, charSet, expiresAt },
+    update: { email, secret, period, algorithm, digits, charSet, expiresAt },
+  });
+  return otp;
+}
+
+/**
  * Prepares users onboarding. Expects email in request form data.
  *
  * If no valid email address is in the form data, it returns an error.
@@ -40,8 +85,8 @@ export async function signup(request: Request) {
   const formData = await request.formData();
   const email = String(formData.get('email'));
 
-  const valid = await isKnownEmail(email);
-  if (!valid) {
+  const user = await getUserByEmail(email);
+  if (!user) {
     await sendErrorMail({
       subject: 'Signup error with invalid email',
       text: `Invalid email address: ${email}`,
@@ -50,6 +95,9 @@ export async function signup(request: Request) {
       errors: { email: 'Unbekannte Email-Adresse. Wende dich an Micha.' },
     };
   }
+
+  const code = await createLoginCode(email);
+  await sendCodeMail({ userName: user.name, code, email });
 
   throw redirect('/onboarding');
 }
