38ee833cf6cc51fd9b2cb01cdc08dea6589438e2
Micha <micha@haus23.net>
2024-10-02 23:42:23 +0200

    feat: Add first app color

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-app">
         {children}
         <ScrollRestoration />
         <Scripts />
