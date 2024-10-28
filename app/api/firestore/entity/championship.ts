e886675e036115ffb0d0472c6b0ec5c101589960
Micha <micha@haus23.net>
2024-10-28 09:29:21 +0100

    feat: Add firestore championship model.

---
+++ app/api/firestore/entity/championship.ts
@@ -0,0 +1,9 @@
+export type FirestoreChampionship = {
+  id: string;
+  name: string;
+  nr: number;
+  rulesId: string;
+  published: boolean;
+  completed: boolean;
+  extraPointsPublished: boolean;
+};
